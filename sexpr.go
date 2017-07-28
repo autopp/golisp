c4f7055301960bb0a1084c62c9fefb84c1e4a6cd
autopp <autopp.inc@gmail.com>
2017-07-28 09:45:13 +0900

    run gofmt for sexpr.go

--- sexpr.go
+++ sexpr.go
@@ -1,63 +1,63 @@
 package golisp
 
 type SExpr interface {
-  isNil() bool
-  isAtom() bool
-  isList() bool
+	isNil() bool
+	isAtom() bool
+	isList() bool
 }
 
 type Bool bool
 type Number int
 type Symbol string
-type Nil struct {}
+type Nil struct{}
 type Cons struct {
-  Car, Cdr SExpr
+	Car, Cdr SExpr
 }
 
-func (sexpr Bool)isNil() bool {
-  return false
+func (sexpr Bool) isNil() bool {
+	return false
 }
 
-func (sexpr Bool)isAtom() bool {
-  return true
+func (sexpr Bool) isAtom() bool {
+	return true
 }
 
-func (sexpr Bool)isList() bool {
-  return false
+func (sexpr Bool) isList() bool {
+	return false
 }
 
-func (sexpr Number)isNil() bool {
-  return false
+func (sexpr Number) isNil() bool {
+	return false
 }
 
-func (sexpr Number)isAtom() bool {
-  return true
+func (sexpr Number) isAtom() bool {
+	return true
 }
 
-func (sexpr Number)isList() bool {
-  return false
+func (sexpr Number) isList() bool {
+	return false
 }
 
-func (sexpr Symbol)isNil() bool {
-  return false
+func (sexpr Symbol) isNil() bool {
+	return false
 }
 
-func (sexpr Symbol)isAtom() bool {
-  return true
+func (sexpr Symbol) isAtom() bool {
+	return true
 }
 
-func (sexpr Symbol)isList() bool {
-  return false
+func (sexpr Symbol) isList() bool {
+	return false
 }
 
-func (sexpr *Nil)isNil() bool {
-  return false
+func (sexpr *Nil) isNil() bool {
+	return false
 }
 
-func (sexpr *Nil)isAtom() bool {
-  return true
+func (sexpr *Nil) isAtom() bool {
+	return true
 }
 
-func (sexpr *Nil)isList() bool {
-  return true
+func (sexpr *Nil) isList() bool {
+	return true
 }
