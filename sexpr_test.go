c4bf5843718b2409803867fb4831ecc566989524
autopp <autopp.inc@gmail.com>
2017-07-28 09:57:01 +0900

    add test for Bool.IsAtom()

--- sexpr_test.go
+++ sexpr_test.go
@@ -7,13 +7,20 @@ import (
 func TestBoolIsNil(t *testing.T) {
   var b Bool
   if b.IsNil() {
-    t.Fatal("Bool.IsNil should be false")
+    t.Fatal("Bool.IsNil() should be false")
+  }
+}
+
+func TestBoolIsAtom(t *testing.T) {
+  var b Bool
+  if !b.IsAtom() {
+    t.Fatal("Bool.IsAtom() should be true")
   }
 }
 
 func TestNumberIsNil(t *testing.T) {
   var n Number
   if n.IsNil() {
-    t.Fatal("Number.IsNil should be false")
+    t.Fatal("Number.IsNil() should be false")
   }
 }
