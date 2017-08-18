47384a38cbbef8d9a34fab17502ea5cc6b2e3a58
autopp <autopp.inc@gmail.com>
2017-08-18 09:38:09 +0900

    impl Env.Define()

--- env.go
+++ env.go
@@ -1,6 +1,23 @@
 package golisp
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Env struct {
-	Prev *Env
-	Body map[string]SExpr
+	body map[string]SExpr
+	prev *Env
+}
+
+func NewEnv(prev *Env) *Env {
+	return &Env{make(map[string]SExpr), prev}
+}
+
+func (e *Env) Define(k string, v SExpr) error {
+	if v, exists := e.body[k]; exists {
+		return errors.New(fmt.Sprintf("%s is already defined with %s", k, v))
+	}
+	e.body[k] = v
+	return nil
 }
--- env_test.go
+++ env_test.go
@@ -1,1 +1,32 @@
 package golisp
+
+import (
+	"testing"
+)
+
+func TestEnvDefine(t *testing.T) {
+	cases := []struct {
+		k   string
+		v   SExpr
+		err bool
+	}{
+		{"foo", False, false},
+		{"bar", True, false},
+		{"foo", True, true},
+	}
+	e := NewEnv(nil)
+
+	for _, tt := range cases {
+		err := e.Define(tt.k, tt.v)
+
+		if tt.err {
+			if err == nil {
+				t.Errorf("%v.Define(%q, %s) == nil, want error", e, tt.k, tt.v)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%v.Define(%q, %s) == %s, want nil", e, tt.k, tt.v, err)
+			}
+		}
+	}
+}
