ece7ed9f29f5f45db4acb1f9a2382b848e18e913
autopp <autopp.inc@gmail.com>
2017-08-24 23:42:23 +0900

    SpForm impl SExpr

--- proc.go
+++ proc.go
@@ -1,5 +1,7 @@
 package golisp
 
+import "fmt"
+
 type Proc interface {
 	Call(args []SExpr, env *Env) (SExpr, error)
 	Name() string
@@ -25,6 +27,26 @@ func (b *procBase) Optional() int {
 	return b.optional
 }
 
+func (b *procBase) IsNil() bool {
+	return false
+}
+
+func (b *procBase) IsAtom() bool {
+	return false
+}
+
+func (b *procBase) IsList() bool {
+	return false
+}
+
+func (b *procBase) IsCons() bool {
+	return false
+}
+
+func (b *procBase) IsProc() bool {
+	return true
+}
+
 type SpForm struct {
 	*procBase
 	Body func([]SExpr, *Env) (SExpr, error)
@@ -32,6 +54,10 @@ type SpForm struct {
 
 func NewSpForm(name string, required, optional int, body func([]SExpr, *Env) (SExpr, error)) *SpForm {
 	return &SpForm{procBase: &procBase{name: name, required: required, optional: optional}, Body: body}
+}
+
+func (s *SpForm) String() string {
+	return fmt.Sprintf("#<special %s>", s.Name())
 }
 
 type Func struct {
